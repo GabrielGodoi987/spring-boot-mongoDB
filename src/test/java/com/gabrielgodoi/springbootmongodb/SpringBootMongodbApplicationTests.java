21b4b5748d36cf1e831ac089388d148fbb80c885
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-03-07 12:22:25 -0300

    :tada: initial commit

---
+++ src/test/java/com/gabrielgodoi/springbootmongodb/SpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielgodoi.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
