c05ffd435a8e9c3bcc7aada91164212860d241de
gabrielgodoi987 <gabrielgodoi452@gmail.com>
2025-03-08 08:48:57 -0300

    learning and using the DTO pattern to transfer data through the application

--- src/main/java/com/gabrielgodoi/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/gabrielgodoi/springbootmongodb/resources/UserResource.java
@@ -1,14 +1,12 @@
 package com.gabrielgodoi.springbootmongodb.resources;
 
 import com.gabrielgodoi.springbootmongodb.domain.User;
+import com.gabrielgodoi.springbootmongodb.dto.UserDto;
 import com.gabrielgodoi.springbootmongodb.services.UserService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -19,8 +17,9 @@ public class UserResource {
     private UserService userService;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDto>> findAll() {
         List<User> userList = this.userService.findAll();
-        return ResponseEntity.ok().body(userList);
+        List<UserDto> userDtoList = userList.stream().map(UserDto::new).toList();
+        return ResponseEntity.ok().body(userDtoList);
     }
 }
